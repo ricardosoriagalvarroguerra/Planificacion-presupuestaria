35f77a1e8d1d332b3e8333657b924d545f41ce06
ricardosoriagalvarroguerra <110310949+ricardosoriagalvarroguerra@users.noreply.github.com>
2025-01-27 20:51:02 -0400

    Create dppValores.js

---
+++ dppValores.js
@@ -0,0 +1,21 @@
+// src/constants/dppValores.js
+
+export const DPP_VALORES = {
+  VPD: { misiones: 168000, consultorias: 130000 },
+  VPO: { misiones: 434707, consultorias: 250000 },
+  VPF: { misiones: 138600, consultorias: 200000 },
+  VPE: { misiones: 28244,  consultorias: 179446 },
+  PRE: { misiones: 0,      consultorias: 0 },
+};
+
+export const DPP_GC_MIS_PER = {
+  VPD: 36960,
+  VPO: 48158,
+  VPF: 40960
+};
+
+export const DPP_GC_MIS_CONS = {
+  VPD: 24200,
+  VPO: 13160,
+  VPF: 24200
+};
